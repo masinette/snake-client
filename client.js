05d6e741998e797e8f1766dd90bc0bb4108230df
masinette <samuelstoniann@gmail.com>
2021-02-10 19:47:11 -0500

    refactored code, and cleaned up a bit, clogs

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require('net');
 const stdin = process.stdin;
-const name = "TSK";
+const { IP, PORT } = require('./constants');
 
 /**
  * Establishes connection with the game server
@@ -8,50 +8,13 @@ const name = "TSK";
 
 const connect = function() {
   const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50542
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
-  //send the server a message with your name
-  // conn.write(JSON.stringify({ name }));
-  // stdin.on('data', (input) => {
-  //   conn.write(JSON.stringify({ name, input }));
-  // });
-
-
-  //attaches an event handler to handle incoming data, and conlog it for the player
-  conn.on('data', (data) => {
-    let parsedData = JSON.parse(data);
-    console.log("\x1b[34m", `${parsedData.name} Said: `, parsedData.input);
-  });
-
-  //send name to snake board upon connection
-  conn.on('connect', (connect) => {
-    // console.log("Successfully connected to game server");
-
-    // conn.write(name);
-    // setInterval(() => {
-    //   conn.write("Move: down");
-    // }, 50);
-
-  });
-
   return conn;
 };
-
-/*
-Vasily server
-PORT: 50542
-HOST: 135.23.222.131
-*/
-
-/*
-"Move: up" - move up one square (unless facing down)
-"Move: down" - move down one square (unless facing up)
-"Move: left" - move left one square (unless facing right)
-"Move: right" - move left one square (unless facing left)
-*/
 
 module.exports = { connect };
--- play.js
+++ play.js
@@ -1,6 +1,5 @@
 const { connect } = require('./client');
 const { setupInput } = require('./input');
-const name = "SNAKE";
 
 console.log('Connecting ...');
 // connect();
